ceb66aca738a3b171fc53e0e437e7529c0cce302
khadafizubaidi <khadafizubaidi.dev@gmail.com>
2024-11-23 19:09:35 +0800

    Add 3rd commit at fake-news-project-streamlit

--- fake-news-project-streamlit/app.py
+++ fake-news-project-streamlit/app.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
-st.write("Hello World")
+def main():
+    st.title("Final Project Nexus Squad")
+    st.write("Selamat datang di halaman Final Project Nexus Squad. Tema final project yang dikerjakan adalah tentang Fake News Detection. Project ini dikerjakan sebagai tugas akhir dari kelompok Nexus Squad. Kelompok Nexus Squad merupakan kelompok 3 pada kelas Data Science & AI Fundamentals Batch 42 yang diselenggarakan oleh DigitalSkola.")
+
+if __name__ == "__main__":
+    main()

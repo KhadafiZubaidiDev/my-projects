c6abbeed5a21231660f73e4fafe62ae2bdfa6014
khadafizubaidi <khadafizubaidi.dev@gmail.com>
2024-11-24 05:38:00 +0800

    Add 4th commit at fake-news-project-streamlit

--- fake-news-project-streamlit/app.py
+++ fake-news-project-streamlit/app.py
@@ -1,8 +1,27 @@
 import streamlit as st
 
-def main():
-    st.title("Final Project Nexus Squad")
-    st.write("Selamat datang di halaman Final Project Nexus Squad. Tema final project yang dikerjakan adalah tentang Fake News Detection. Project ini dikerjakan sebagai tugas akhir dari kelompok Nexus Squad. Kelompok Nexus Squad merupakan kelompok 3 pada kelas Data Science & AI Fundamentals Batch 42 yang diselenggarakan oleh DigitalSkola.")
+st.set_page_config(
+    page_title="Nexus Squad",
+    page_icon=":house:",
+)
 
-if __name__ == "__main__":
-    main()
+st.write("# Fake News Detection")
+st.sidebar.warning("Created by Nexus Squad")
+
+st.markdown(
+    """
+    Selamat datang di Aplikasi Fake News Detection.
+    Aplikasi ini disusun sebagai Final Project pada kelas Data Science & AI Fundamental Batch 42 yang diselenggarakan oleh DigitalSkola.
+    ### Pengantar
+    Sebagaimana yang diketahui bahwa era digital membawa dampak yang besar pada kehidupan manusia, mulai dari penyebaran informasi dan lain sebagainya. 
+    Dari segi penyebaran informasi seperti kita ketahui bersama informasi dapat diakses dengan cepat dan mudah, baik untuk mencari materi pelajaran, jurnal ilmiah, maupun sumber belajar lainnya.
+    Dengan penyebaran informasi yang begitu cepat dan masif, kita sebagai penerima informasi tersebut tidak sempat melakukan pengecekan terhadap informasi tersebut.
+    Sehingga berpengaruh secara tidak langsung kepada keputusan yang diambil berdasarkan informasi tersebut.
+    \n
+    Untuk itu diperlukan suatu upaya untuk mengecek informasi tersebut apakah merupakan informasi asli atau bukan. 
+    Salah satu cara untuk mengecek informasi tersebut adalah dengan menggunakan algoritma machine learning dengan menggunakan dataset yang telah disediakan.
+    \n
+    Akhirnya kami selaku penyusun mengucapkan terima kasih atas semua yang telah membantu kami dalam membuat aplikasi ini, terutama dari para mentor dan anggota tim yang telah membantu penyusunan aplikasi ini dari awal hingga akhir.
+    Semoga aplikasi ini bermanfaat. 
+"""
+)
